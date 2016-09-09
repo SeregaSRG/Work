78026903064824471165995cdc40f7f2817a281a
SeregaSRG <monohrom98@gmail.com>
2016-09-09 21:46:20 +0300

    Clean

--- сoreOLD/libraries/response.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Response{
-    static function send($struct = null){
-        echo json_encode(array('status' => 'done','data' => $struct));
-    }
-    static function error($errorCode = null){
-        echo json_encode(array('status' => 'error','errorcode' => $errorCode));
-    }
-}
